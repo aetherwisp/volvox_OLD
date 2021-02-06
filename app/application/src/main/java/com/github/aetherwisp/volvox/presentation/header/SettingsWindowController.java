6d8db16c6e07647af418b6d41b7a703d9c7b0be5
aetherwisp <aetherwisp@outlook.jp>
2021-02-06 21:53:04 +0900

    Add SettingsWindowController.

---
+++ app/application/src/main/java/com/github/aetherwisp/volvox/presentation/header/SettingsWindowController.java
@@ -0,0 +1,18 @@
+package com.github.aetherwisp.volvox.presentation.header;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.apache.logging.log4j.LogManager.getLogger;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/header")
+public class SettingsWindowController {
+    //======================================================================
+    // Fields
+    private static final Logger logger = getLogger(lookup().lookupClass());
+
+    //======================================================================
+    // Methods
+}
